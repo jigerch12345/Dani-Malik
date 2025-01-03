91a998928010a29a9808f3f50dd7ddc81b87c877
jigerch12345 <jigerch17@gmail.com>
2025-01-03 23:06:10 +0500

    Update dpcute.js

--- models/commands/dpcute.js
+++ models/commands/dpcute.js
@@ -1,27 +1,74 @@
 module.exports.config = {
-  name: "dpcute",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "SHAAN KHAN",
-  description: "Dp cute",
-  commandCategory: "Random-IMG",
-  usages: "cute dp",
-  cooldowns: 2,
-  dependencies: {
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
-
+	name: "Johnny",
+	version: "1.0.1",
+	hasPermssion: 0,
+	credits: "Mr Amir",
+	description: "Comment on the board",
+	commandCategory: "game",
+	usages: "[text]",
+	cooldowns: 5,
+	dependencies: {
+		"canvas":"",
+		 "axios":"",
+		 "fs-extra":""
+	}
 };
 
-module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
-const axios = global.nodemodule["axios"];
-const request = global.nodemodule["request"];
-const fs = global.nodemodule["fs-extra"];
-    var link = [
-"https://i.imgur.com/9lidHFg.jpg","https://i.imgur.com/qrairnJ.jpg","https://i.imgur.com/VHjp32X.jpg","https://i.imgur.com/OzGaHPf.jpg","https://i.imgur.com/VSaGosg.jpg","https://i.imgur.com/YGbFYAj.jpg","https://i.imgur.com/Rwf3mjD.jpg","https://i.imgur.com/hrs30UR.jpg","https://i.imgur.com/k6HIZGe.jpg","https://i.imgur.com/myXdpx3.jpg","https://i.imgur.com/Z5ycqK5.jpg","https://i.imgur.com/llGEE3b.jpg","https://i.imgur.com/xjXbvsO.jpg","https://i.imgur.com/o1pKJC5.jpg ",
-     ];
-     var callback = () => api.sendMessage({body:`┏━━━━━┓\n     ❣️𝐒𝐇𝐀𝐀𝐍-𝐊𝐇𝐀𝐍💞                    ✧══•❁😛❁•══✧\n┗━━━━━┛\n\n♥️`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
-      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
+module.exports.wrapText = (ctx, text, maxWidth) => {
+	return new Promise(resolve => {
+		if (ctx.measureText(text).width < maxWidth) return resolve([text]);
+		if (ctx.measureText('W').width > maxWidth) return resolve(null);
+		const words = text.split(' ');
+		const lines = [];
+		let line = '';
+		while (words.length > 0) {
+			let split = false;
+			while (ctx.measureText(words[0]).width >= maxWidth) {
+				const temp = words[0];
+				words[0] = temp.slice(0, -1);
+				if (split) words[1] = `${temp.slice(-1)}${words[1]}`;
+				else {
+					split = true;
+					words.splice(1, 0, temp.slice(-1));
+				}
+			}
+			if (ctx.measureText(`${line}${words[0]}`).width < maxWidth) line += `${words.shift()} `;
+			else {
+				lines.push(line.trim());
+				line = '';
+			}
+			if (words.length === 0) lines.push(line.trim());
+		}
+		return resolve(lines);
+	});
+} 
+
+module.exports.run = async function({ api, event, args }) {
+	let { senderID, threadID, messageID } = event;
+	const { loadImage, createCanvas } = require("canvas");
+	const fs = global.nodemodule["fs-extra"];
+	const axios = global.nodemodule["axios"];
+	let pathImg = __dirname + '/cache/markngu.png';
+	var text = args.join(" ");
+	if (!text) return api.sendMessage("Enter the content of the comment on the board", threadID, messageID);
+	let getPorn = (await axios.get(`https://i.imgur.com/J1YaQ6I.jpeg`, { responseType: 'arraybuffer' })).data;
+	fs.writeFileSync(pathImg, Buffer.from(getPorn, 'utf-8'));
+	let baseImage = await loadImage(pathImg);
+	let canvas = createCanvas(baseImage.width, baseImage.height);
+	let ctx = canvas.getContext("2d");
+	ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+	ctx.font = "400 45px Arial";
+	ctx.fillStyle = "#000000";
+	ctx.textAlign = "start";
+	let fontSize = 45;
+	while (ctx.measureText(text).width > 2250) {
+		fontSize--;
+		ctx.font = `400 ${fontSize}px Arial, sans-serif`;
+	}
+	const lines = await this.wrapText(ctx, text, 440);
+	ctx.fillText(lines.join('\n'), 95,420);//comment
+	ctx.beginPath();
+	const imageBuffer = canvas.toBuffer();
+	fs.writeFileSync(pathImg, imageBuffer);
+return api.sendMessage({ attachment: fs.createReadStream(pathImg) }, threadID, () => fs.unlinkSync(pathImg), messageID);        
 }
