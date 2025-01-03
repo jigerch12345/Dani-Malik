74e62dc2ba89fceeabebdf0d6aa6c9081c2db8a0
jigerch12345 <jigerch17@gmail.com>
2025-01-03 23:01:06 +0500

    Update autoseen.js

--- models/commands/autoseen.js
+++ models/commands/autoseen.js
@@ -1,1 +1,112 @@
-const _0x2e090f=_0x1204;(function(_0x2b7be7,_0x1a99ed){const _0x28d2c9=_0x1204,_0x154c6b=_0x2b7be7();while(!![]){try{const _0x435efb=-parseInt(_0x28d2c9(0x134))/0x1+-parseInt(_0x28d2c9(0x12b))/0x2+parseInt(_0x28d2c9(0x144))/0x3+-parseInt(_0x28d2c9(0x137))/0x4+parseInt(_0x28d2c9(0x138))/0x5+-parseInt(_0x28d2c9(0x13c))/0x6+-parseInt(_0x28d2c9(0x131))/0x7*(-parseInt(_0x28d2c9(0x13b))/0x8);if(_0x435efb===_0x1a99ed)break;else _0x154c6b['push'](_0x154c6b['shift']());}catch(_0x48b087){_0x154c6b['push'](_0x154c6b['shift']());}}}(_0xcdcc,0xdc861));const fs=require(_0x2e090f(0x13a)),pathFile=__dirname+_0x2e090f(0x124);function _0x1204(_0xcb9d69,_0x52f842){const _0xcdcc35=_0xcdcc();return _0x1204=function(_0x1204e0,_0xb97b23){_0x1204e0=_0x1204e0-0x124;let _0x42ad38=_0xcdcc35[_0x1204e0];return _0x42ad38;},_0x1204(_0xcb9d69,_0x52f842);}if(!fs[_0x2e090f(0x141)](pathFile))fs['writeFileSync'](pathFile,_0x2e090f(0x143));module[_0x2e090f(0x13f)][_0x2e090f(0x12f)]={'name':_0x2e090f(0x126),'version':_0x2e090f(0x132),'hasPermssion':0x2,'credits':'Dev\x20Sy','description':_0x2e090f(0x135),'commandCategory':_0x2e090f(0x13d),'usages':_0x2e090f(0x130),'cooldowns':0x5},module['exports']['handleEvent']=async({api:_0x4d275c,event:_0x48456e,args:_0x5c7f33})=>{const _0x981e34=_0x2e090f,_0x292c3b=fs[_0x981e34(0x125)](pathFile,'utf-8');if(_0x292c3b==_0x981e34(0x133))_0x4d275c[_0x981e34(0x139)](()=>{});},module['exports'][_0x2e090f(0x146)]=async({api:_0x187810,event:_0x5d637,args:_0x2d79a7})=>{const _0x4fcf71=_0x2e090f;try{if(_0x2d79a7[0x0]=='on')fs[_0x4fcf71(0x127)](pathFile,_0x4fcf71(0x133)),_0x187810[_0x4fcf71(0x12a)](this[_0x4fcf71(0x12f)][_0x4fcf71(0x145)]+_0x4fcf71(0x136),_0x5d637['threadID'],_0x5d637[_0x4fcf71(0x140)]);else _0x2d79a7[0x0]==_0x4fcf71(0x13e)?(fs[_0x4fcf71(0x127)](pathFile,'false'),_0x187810['sendMessage'](this[_0x4fcf71(0x12f)][_0x4fcf71(0x145)]+_0x4fcf71(0x12d),_0x5d637[_0x4fcf71(0x12c)],_0x5d637[_0x4fcf71(0x140)])):_0x187810['sendMessage'](_0x4fcf71(0x142)+global[_0x4fcf71(0x12f)][_0x4fcf71(0x129)]+this[_0x4fcf71(0x12f)][_0x4fcf71(0x145)]+'\x20'+this[_0x4fcf71(0x12f)][_0x4fcf71(0x128)],_0x5d637[_0x4fcf71(0x12c)],_0x5d637[_0x4fcf71(0x140)]);}catch(_0x31fa7e){console[_0x4fcf71(0x12e)](_0x31fa7e);}};function _0xcdcc(){const _0x23ebc6=['on/off','5453VyzxCv','1.0.0','true','1160828XWgzVs','Auto\x20seen','\x20turn\x20on\x20successfully.','5952016EcuIjn','5239690sIeOlM','markAsReadAll','fs-extra','39112FDUOfo','7371108kwngOw','tools','off','exports','messageID','existsSync','Wrong\x20format\x0aUse\x20','false','2323053dklQuS','name','run','/cache/autoseen.txt','readFileSync','autoseen','writeFileSync','usages','PEFIX','sendMessage','1700410jMePMC','threadID','\x20turn\x20off\x20successfully','log','config'];_0xcdcc=function(){return _0x23ebc6;};return _0xcdcc();}
+module.exports.config = {
+  name: "eng",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "Khoa",
+  description: "It's a compound :>",
+  usePrefix: true,
+  commandCategory: "fun",
+  usages: "",
+  dependencies: {
+        "axios": "",
+        "fs-extra": ""
+  },
+  cooldowns: 15
+}
+
+module.exports.run = async function ({ args, Users, Threads, api, event, Currencies }) {
+  const { loadImage, createCanvas } = require("canvas");
+  const fs = global.nodemodule["fs-extra"];
+  const axios = global.nodemodule["axios"];
+  let pathImg = __dirname + "/cache/background.png";
+  let pathAvt1 = __dirname + "/cache/Avtmot.png";
+  let pathAvt2 = __dirname + "/cache/Avthai.png";
+  
+  var id1 = event.senderID;
+  var name1 = await Users.getNameUser(id1);
+  var ThreadInfo = await api.getThreadInfo(event.threadID);
+  var all = ThreadInfo.userInfo
+  for (let c of all) {
+    if (c.id == id1) var gender1 = c.gender;
+  };
+  const botID = api.getCurrentUserID();
+  let ungvien = [];
+  if(gender1 == "FEMALE"){
+    for (let u of all) {
+      if (u.gender == "MALE") {
+      if (u.id !== id1 && u.id !== botID) ungvien.push(u.id)
+      }
+    }
+  }
+  else if(gender1 == "MALE"){
+    for (let u of all) {
+      if (u.gender == "FEMALE") {
+      if (u.id !== id1 && u.id !== botID) ungvien.push(u.id)
+      }
+    }
+  }
+  else {
+  for (let u of all) {
+      if (u.id !== id1 && u.id !== botID) ungvien.push(u.id)
+    }
+  }
+  var id2 = ungvien[Math.floor(Math.random() * ungvien.length)];
+  var name2 = await Users.getNameUser(id2);
+  var rd1 = Math.floor(Math.random() * 100) + 1;
+  var cc = ["0", "-1", "99,99", "-99", "-100", "101", "0,01"];
+  var rd2 = cc[Math.floor(Math.random() * cc.length)];
+  var djtme = [`${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd2}`, `${rd1}`, `${rd1}`, `${rd1}`, `${rd1}`];
+  
+  var tile = djtme[Math.floor(Math.random() * djtme.length)];
+
+  var background = [
+  "https://i.postimg.cc/wjJ29HRB/background1.png",
+  "https://i.postimg.cc/zf4Pnshv/background2.png",
+  "https://i.postimg.cc/5tXRQ46D/background3.png"
+  ];
+  var rd = background[Math.floor(Math.random() * background.length)];
+  
+  let getAvtmot = (
+    await axios.get(
+      `https://graph.facebook.com/${id1}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
+      { responseType: "arraybuffer" }
+    )
+  ).data;
+  fs.writeFileSync(pathAvt1, Buffer.from(getAvtmot, "utf-8"));
+
+  let getAvthai = (
+    await axios.get(
+      `https://graph.facebook.com/${id2}/picture?width=720&height=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
+      { responseType: "arraybuffer" }
+    )
+  ).data;
+  fs.writeFileSync(pathAvt2, Buffer.from(getAvthai, "utf-8"));
+
+  let getbackground = (
+    await axios.get(`${rd}`, {
+      responseType: "arraybuffer",
+    })
+  ).data;
+  fs.writeFileSync(pathImg, Buffer.from(getbackground, "utf-8"));
+
+  let baseImage = await loadImage(pathImg);
+  let baseAvt1 = await loadImage(pathAvt1);
+  let baseAvt2 = await loadImage(pathAvt2);
+  let canvas = createCanvas(baseImage.width, baseImage.height);
+  let ctx = canvas.getContext("2d");
+  ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);
+  ctx.drawImage(baseAvt1, 100, 150, 300, 300);
+  ctx.drawImage(baseAvt2, 900, 150, 300, 300);
+  const imageBuffer = canvas.toBuffer();
+  fs.writeFileSync(pathImg, imageBuffer);
+  fs.removeSync(pathAvt1);
+  fs.removeSync(pathAvt2);
+  return api.sendMessage({ body: `Congratulations💫🎉, ${name1} Your Engagement Has Fixed with ${name2}\nBe HappY AlwaYs🖤`,
+            mentions: [{
+          tag: `${name2}`,
+          id: id2
+        }], attachment: fs.createReadStream(pathImg) },
+      event.threadID,
+      () => fs.unlinkSync(pathImg),
+      event.messageID);
+}
