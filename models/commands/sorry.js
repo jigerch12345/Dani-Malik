4098a643ea2a492f9e17a2383857c807a73c7eed
jigerch12345 <jigerch17@gmail.com>
2025-01-03 23:13:04 +0500

    Update sorry.js

--- models/commands/sorry.js
+++ models/commands/sorry.js
@@ -1,1 +1,75 @@
-var _0x320b32=_0x41e7;function _0x41e7(_0x52b4af,_0x32c2fd){var _0x33a6a1=_0x33a6();return _0x41e7=function(_0x41e740,_0x3a5941){_0x41e740=_0x41e740-0xcd;var _0x4740b3=_0x33a6a1[_0x41e740];return _0x4740b3;},_0x41e7(_0x52b4af,_0x32c2fd);}function _0x33a6(){var _0x2e05a1=['Mujhe\x20Sab\x20Pata\x20Tha','53MGglyq','63sQEkvD','Aur\x20Dher\x20Sara\x20Enjoy\x20karo\x20aj','2154224IFjqcs','sorry','Ajj\x20Tumhe\x20Happy\x20Rehna\x20Chaiye','3115580FzAwQI','...\x20-\x20Long\x20LTD','12UcIFdp','bold\x20war','Lekin\x20Kya\x20kare\x20Mera\x20Luck\x20Hi\x20Kharab\x20ha','please\x20🥺','But\x20The\x20Thing\x20Is','Khush\x20Raho\x20Tum','106782rKjECe','Mujhe\x20Pata\x20nhi\x20Tha\x20Kuch\x20Aisa\x20Hoga','War\x20nát\x20cái\x20boxchat','10vSkjBZ','please','So,\x20Apna\x20Face\x20pe\x20Smile\x20Rakkho','run','I\x27m\x20So\x20So\x20Sorry\x20Dear\x20Good\x20Bye🥺💖','1714036zlSeWt','Jaan\x20plz\x20na','mentions','threadID','exports','9848839wgBpwo','553WfJkUN','3STlEIc','push','sendMessage','Aur\x20Har\x20Waqt\x20Haste\x20Rao','My\x20Dear\x20Janu','70210uLjKnT','Please\x20Bura\x20Mat\x20Mano','Maano\x20na','I\x20Know\x20I\x27m\x20So\x20Late','I\x27m\x20So\x20So\x20Sorry','56228731kYWyWx'];_0x33a6=function(){return _0x2e05a1;};return _0x33a6();}(function(_0x59407f,_0x2c30b0){var _0x1b68e3=_0x41e7,_0x1547da=_0x59407f();while(!![]){try{var _0x4cca62=-parseInt(_0x1b68e3(0xdb))/0x1*(parseInt(_0x1b68e3(0xd4))/0x2)+parseInt(_0x1b68e3(0xcf))/0x3*(-parseInt(_0x1b68e3(0xf1))/0x4)+-parseInt(_0x1b68e3(0xe1))/0x5+-parseInt(_0x1b68e3(0xe9))/0x6*(parseInt(_0x1b68e3(0xce))/0x7)+-parseInt(_0x1b68e3(0xde))/0x8*(-parseInt(_0x1b68e3(0xdc))/0x9)+parseInt(_0x1b68e3(0xec))/0xa*(-parseInt(_0x1b68e3(0xcd))/0xb)+parseInt(_0x1b68e3(0xe3))/0xc*(parseInt(_0x1b68e3(0xd9))/0xd);if(_0x4cca62===_0x2c30b0)break;else _0x1547da['push'](_0x1547da['shift']());}catch(_0x2c9901){_0x1547da['push'](_0x1547da['shift']());}}}(_0x33a6,0xf357b),module[_0x320b32(0xf5)]['config']={'name':_0x320b32(0xdf),'version':'1.0.0','hasPermssion':0x0,'credits':_0x320b32(0xe2),'description':_0x320b32(0xeb),'commandCategory':'group','usages':_0x320b32(0xe4),'cooldowns':0xa,'dependencies':{'fs-extra':'','axios':''}},module['exports'][_0x320b32(0xef)]=async function({api:_0x9da0f0,args:_0x2019b0,Users:_0x4b4c9e,event:_0x5f31e1}){var _0x37e592=_0x320b32,_0x52023f=Object['keys'](_0x5f31e1[_0x37e592(0xf3)])[0x0];let _0x51fca9=_0x5f31e1[_0x37e592(0xf3)][_0x52023f];var _0x49a689=[];_0x49a689[_0x37e592(0xd0)]({'id':_0x52023f});var _0x15545a=function(_0x21f02f){var _0x160b2a=_0x37e592;_0x9da0f0[_0x160b2a(0xd1)](_0x21f02f,_0x5f31e1[_0x160b2a(0xf4)]);};_0x15545a(_0x37e592(0xd8)),setTimeout(()=>{_0x15545a({'body':'Maf\x20Kardo\x20Pls'});},0xbb8),setTimeout(()=>{var _0x152066=_0x37e592;_0x15545a({'body':_0x152066(0xed)});},0x1388),setTimeout(()=>{var _0xba8613=_0x37e592;_0x15545a({'body':_0xba8613(0xe6)});},0x1b58),setTimeout(()=>{_0x15545a({'body':'please\x20😞'});},0x2328),setTimeout(()=>{var _0x15bae5=_0x37e592;_0x15545a({'body':_0x15bae5(0xd7)});},0x2ee0),setTimeout(()=>{_0x15545a({'body':'I\x20Know\x20I\x27m\x20Care\x20less'});},0x3a98),setTimeout(()=>{var _0x20bbe1=_0x37e592;_0x15545a({'body':_0x20bbe1(0xe7)});},0x4268),setTimeout(()=>{var _0x5ed483=_0x37e592;_0x15545a({'body':_0x5ed483(0xda)});},0x4e20),setTimeout(()=>{_0x15545a({'body':'M\x20Agar\x20Chahta\x20Toh\x20Subha\x20Hi\x20Bol\x20Sakta\x20tha'});},0x59d8),setTimeout(()=>{_0x15545a({'body':'Lekin\x20M\x20Buddhu\x20Tha\x20Nhi\x20kiya'});},0x61a8),setTimeout(()=>{var _0x41e381=_0x37e592;_0x15545a({'body':_0x41e381(0xea)});},0x6f54),setTimeout(()=>{var _0x576b25=_0x37e592;_0x15545a({'body':_0x576b25(0xe5)});},0x7918),setTimeout(()=>{var _0x48c5f0=_0x37e592;_0x15545a({'body':_0x48c5f0(0xd5)});},0x8ca0),setTimeout(()=>{var _0x5c8e94=_0x37e592;_0x15545a({'body':_0x5c8e94(0xdd)});},0x9858),setTimeout(()=>{var _0x438e5e=_0x37e592;_0x15545a({'body':_0x438e5e(0xe0)});},0x9c40),setTimeout(()=>{var _0x2ea786=_0x37e592;_0x15545a({'body':_0x2ea786(0xee)});},0xfde8),setTimeout(()=>{var _0x355488=_0x37e592;_0x15545a({'body':_0x355488(0xd2)});},0x11170),setTimeout(()=>{_0x15545a({'body':'Mere\x20Sath\x20Hamesha\x20Raho'});},0x124f8),setTimeout(()=>{var _0x587e30=_0x37e592;_0x15545a({'body':_0x587e30(0xe8)});},0x13880),setTimeout(()=>{var _0x2cc12a=_0x37e592;_0x15545a({'body':_0x2cc12a(0xf2)});},0x14c08),setTimeout(()=>{var _0x50c37c=_0x37e592;_0x15545a(_0x50c37c(0xd6));},0x15f90),setTimeout(()=>{_0x15545a({'body':'Please\x20Ma\x20Sorry\x20Krta\x20Hu\x20Janu\x20'});},0x17318),setTimeout(()=>{var _0x3d4972=_0x37e592;_0x15545a({'body':_0x3d4972(0xd3)});},0x186a0),setTimeout(()=>{_0x15545a({'body':'Upar\x20Wala\x20Apko\x20Hamesha\x20kHush\x20RaKhy'});},0x19a28),setTimeout(()=>{var _0x129ae5=_0x37e592;_0x15545a({'body':_0x129ae5(0xf0)});},0x1c138);});
+module.exports.config = {
+    name: "Pair-tag",
+    version: "3.1.1",
+    hasPermssion: 0,
+    credits: "Mr Amir",
+    description: "Pairing",
+    usePrefix: true,
+    commandCategory: "img",
+    usages: "[@mention]",
+    cooldowns: 5,
+    dependencies: {
+        "axios": "",
+        "fs-extra": "",
+        "path": "",
+        "jimp": ""
+    }
+};
+
+module.exports.onLoad = async() => {
+    const { resolve } = global.nodemodule["path"];
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { downloadFile } = global.utils;
+    const dirMaterial = __dirname + `/cache/canvas/`;
+    const path = resolve(__dirname, 'cache/canvas', 'married.jpeg');
+    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
+    if (!existsSync(path)) await downloadFile("https://i.imgur.com/OUKnbB6.jpeg", path);
+}
+
+async function makeImage({ one, two }) {
+    const fs = global.nodemodule["fs-extra"];
+    const path = global.nodemodule["path"];
+    const axios = global.nodemodule["axios"]; 
+    const jimp = global.nodemodule["jimp"];
+    const __root = path.resolve(__dirname, "cache", "canvas");
+
+    let batgiam_img = await jimp.read(__root + "/married.jpeg");
+    let pathImg = __root + `/batman${one}_${two}.png`;
+    let avatarOne = __root + `/avt_${one}.png`;
+    let avatarTwo = __root + `/avt_${two}.png`;
+    
+    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+    
+    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
+    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
+    
+    let circleOne = await jimp.read(await circle(avatarOne));
+    let circleTwo = await jimp.read(await circle(avatarTwo));
+    batgiam_img.composite(circleOne.resize(200, 200), 72, 107).composite(circleTwo.resize(199, 200), 465, 105);
+    
+    let raw = await batgiam_img.getBufferAsync("image/png");
+    
+    fs.writeFileSync(pathImg, raw);
+    fs.unlinkSync(avatarOne);
+    fs.unlinkSync(avatarTwo);
+    
+    return pathImg;
+}
+async function circle(image) {
+    const jimp = require("jimp");
+    image = await jimp.read(image);
+    image.circle();
+    return await image.getBufferAsync("image/png");
+}
+
+module.exports.run = async function ({ event, api, args }) {    
+    const fs = global.nodemodule["fs-extra"];
+    const { threadID, messageID, senderID } = event;
+    const mention = Object.keys(event.mentions);
+    if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
+    else {
+        const one = senderID, two = mention[0];
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "╔═══❖••° °••❖═══╗\n    𝐒𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥 𝐏𝐚𝐢𝐫𝐢𝐧𝐠\n╚═══❖••° °••❖═══╝\n\n✶⊶⊷⊷❍⊶⊷⊷✶\n         👑 تجھے کتنا ❤️\n        🌸چاہنے لگے ہم 🩷\n✶⊶⊷⊷❍⊶⊷⊷✶", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+    }
+      }
